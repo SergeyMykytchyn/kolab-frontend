317d902bf6523176a77fd3449c529badf90b3b72
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-02-27 19:44:15 +0200

    add routing for landing sign-in and sign-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./routes/LandingPage";
+import SignInPage from "./routes/SignInPage";
+import SignUpPage from "./routes/SignUpPage";
 
 const App = () => {
   return (
     <div className="container">
-      Kolab
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/sign-in" element={<SignInPage />} />
+          <Route exact path="/sign-up" element={<SignUpPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
