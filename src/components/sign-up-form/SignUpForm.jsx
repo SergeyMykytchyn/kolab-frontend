ea4db33d0e5fa353264bec64d439941dc1ae47aa
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-02-28 11:04:20 +0200

    sign-up-form done till select for roles

--- src/components/sign-up-form/SignUpForm.jsx
+++ src/components/sign-up-form/SignUpForm.jsx
@@ -1,78 +1,127 @@
 import React, { useState } from "react";
 import "./SignUpForm.css";
+import TextField from "@mui/material/TextField";
 
 const SignUpForm = () => {
   const [firstName, setFirstName] = useState("");
+  const [firstNameClicked, setFirstNameClicked] = useState(false);
+  
   const [lastName, setLastName] = useState("");
+  const [lastNameClicked, setLastNameClicked] = useState(false);
+
   const [email, setEmail] = useState("");
+  const [emailClicked, setEmailClicked] = useState(false);
+
   const [password, setPassword] = useState("");
+  const [passwordClicked, setPasswordClicked] = useState(false);
+
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordClicked, setConfirmPasswordClicked] = useState(false);
 
   return (
     <form className="sign-up-form">
       <div className="sign-up-card">
         <div className="sign-up-cardTitle"><span class="sign-up-cardTitleText">Sign up</span></div>
         <div>
+
+          <TextField
+            sx={{
+              marginBottom: "8px",
+              ".MuiFormHelperText-root": {
+                visibility: !firstNameClicked || firstName ? "hidden" : "visible"
+              }
+            }}
+            required
+            error={!firstNameClicked || firstName ? false: true}
+            id="filled-error-helper-text-required"
+            label="First name"
+            variant="filled"
+            helperText="First name is required"
+            fullWidth
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+            onBlur={() => setFirstNameClicked(true)}
+          />
           
-          <label for="inp" class="inp">
-            <input
-              value={firstName}
-              onChange={e => setFirstName(e.target.value)}
-              type="text"
-              id="inp"
-              placeholder="&nbsp;"
-            />
-            <span class="label">First name</span>
-            <span class="focus-bg"></span>
-          </label>
+          <TextField
+            sx={{
+              marginBottom: "8px",
+              ".MuiFormHelperText-root": {
+                visibility: !lastNameClicked || lastName ? "hidden" : "visible"
+              }
+            }}
+            required
+            error={!lastNameClicked || lastName ? false: true}
+            id="filled-error-helper-text-required"
+            label="Last name"
+            variant="filled"
+            helperText="Last name is required"
+            fullWidth
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+            onBlur={() => setLastNameClicked(true)}
+          />
 
-          <label for="inp" class="inp">
-            <input
-              value={lastName}
-              onChange={e => setLastName(e.target.value)}
-              type="text"
-              id="inp"
-              placeholder="&nbsp;"
-            />
-            <span class="label">Last name</span>
-            <span class="focus-bg"></span>
-          </label>
+          <TextField
+            sx={{
+              marginBottom: "8px",
+              ".MuiFormHelperText-root": {
+                visibility: !emailClicked || email ? "hidden" : "visible"
+              }
+            }}
+            required
+            error={!emailClicked || email ? false: true}
+            id="filled-error-helper-text-required"
+            label="Email"
+            variant="filled"
+            helperText="Email is required"
+            fullWidth
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            onBlur={() => setEmailClicked(true)}
+          />
 
-          <label for="inp" class="inp">
-            <input
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              type="text"
-              id="inp"
-              placeholder="&nbsp;"
-            />
-            <span class="label">Email</span>
-            <span class="focus-bg"></span>
-          </label>
+          <TextField
+            sx={{
+              marginBottom: "8px",
+              ".MuiFormHelperText-root": {
+                visibility: !passwordClicked || password ? "hidden" : "visible"
+              }
+            }}
+            required
+            error={!passwordClicked || password ? false: true}
+            id="filled-error-helper-text-required"
+            label="Password"
+            variant="filled"
+            helperText="Password is required"
+            fullWidth
+            type="password"
+            autoComplete="new-password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            onBlur={() => setPasswordClicked(true)}
+          />
 
-          <label for="inp" class="inp">
-            <input
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              type="text"
-              id="inp"
-              placeholder="&nbsp;"
-            />
-            <span class="label">Password</span>
-            <span class="focus-bg"></span>
-          </label>
-
-          <label for="inp" class="inp">
-            <input
-              value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-              type="text"
-              id="inp"
-              placeholder="&nbsp;"
-            />
-            <span class="label">Confirm password</span>
-            <span class="focus-bg"></span>
-          </label>
+          <TextField
+            sx={{
+              marginBottom: "8px",
+              ".MuiFormHelperText-root": {
+                visibility: !confirmPasswordClicked || confirmPassword ? "hidden" : "visible"
+              }
+            }}
+            required
+            error={!confirmPasswordClicked || confirmPassword ? false: true}
+            id="filled-error-helper-text-required"
+            label="Confrim password"
+            variant="filled"
+            helperText="Confrim password is required"
+            fullWidth
+            type="password"
+            autoComplete="new-password"
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+            onBlur={() => setConfirmPasswordClicked(true)}
+          />
 
         </div>
       </div>
