a3a81e73782ad8c354b632f9c8965fc1103a13af
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-02-28 20:47:29 +0200

    incorrect data dialog

---
+++ src/components/incorrect-data-dialog/IncorrectDataDialog.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./IncorrectDataDialog.css";
+
+const IncorrectDataDialog = ({ handleOk, title, message }) => {
+  console.log("IncorrectDataDialog");
+  return (
+    <div className="overlay-container">
+      <div className="overlay-backdrop"></div>
+      <div className="global-overlay-wrapper">
+        <div className="overlay-pane">
+          <div className="overlay-pane-title">
+            <span>{title}</span>
+          </div>
+          <div className="overlay-pane-message">
+            <span>{message}</span>
+          </div>
+          <div className="overlay-pane-button-wrapper">
+            <button onClick={() => handleOk()} className="overlay-pane-button">Ok</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IncorrectDataDialog;
--- src/components/sign-in-form/SignInForm.jsx
+++ src/components/sign-in-form/SignInForm.jsx
@@ -3,6 +3,7 @@ import "./SignInForm.css";
 import TextField from "@mui/material/TextField";
 import { useNavigate } from "react-router-dom";
 import Api from "../../api/Api";
+import IncorrectDataDialog from "../incorrect-data-dialog/IncorrectDataDialog";
 
 const SignInForm = () => {
   const history = useNavigate();
@@ -13,6 +14,8 @@ const SignInForm = () => {
   const [password, setPassword] = useState("");
   const [passwordClicked, setPasswordClicked] = useState(false);
 
+  const [isIncorrectData, setIsIncorrectData] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -20,11 +23,11 @@ const SignInForm = () => {
         email,
         password
       });
-      console.log(response.data.token);
       localStorage.setItem("TOKEN", response.data.token);
       history("/groups");
     } catch(err) {
-      console.error(err.message);
+      setIsIncorrectData(true);
+      console.error(err);
     }
   };
 
@@ -34,57 +37,61 @@ const SignInForm = () => {
     }
   }, []);
 
+  console.log(isIncorrectData);
   return (
-    <form className="sign-in-form">
-      <div className="card">
-        <div className="cardTitle"><span className="cardTitleText">Sign in</span></div>
+    <>
+      {isIncorrectData ? <IncorrectDataDialog handleOk={() => setIsIncorrectData(!isIncorrectData)} title="Error" message="Incorrect password or email" /> : null }
+      <form className="sign-in-form">
+        <div className="card">
+          <div className="cardTitle"><span className="cardTitleText">Sign in</span></div>
 
-          <TextField
-            sx={{
-              marginBottom: "8px",
-              ".MuiFormHelperText-root": {
-                visibility: !emailClicked || email ? "hidden" : "visible"
-              }
-            }}
-            required
-            error={!emailClicked || email ? false: true}
-            id="filled-error-helper-text-required"
-            label="Email"
-            variant="filled"
-            helperText="Email is required"
-            fullWidth
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            onBlur={() => setEmailClicked(true)}
-          />
+            <TextField
+              sx={{
+                marginBottom: "8px",
+                ".MuiFormHelperText-root": {
+                  visibility: !emailClicked || email ? "hidden" : "visible"
+                }
+              }}
+              required
+              error={!emailClicked || email ? false: true}
+              id="filled-error-helper-text-required"
+              label="Email"
+              variant="filled"
+              helperText="Email is required"
+              fullWidth
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              onBlur={() => setEmailClicked(true)}
+            />
 
-          <TextField
-            sx={{
-              marginBottom: "8px",
-              ".MuiFormHelperText-root": {
-                visibility: !passwordClicked || password ? "hidden" : "visible"
-              }
-            }}
-            required
-            error={!passwordClicked || password ? false: true}
-            id="filled-error-helper-text-required"
-            label="Password"
-            variant="filled"
-            helperText="Password is required"
-            fullWidth
-            type="password"
-            autoComplete="new-password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            onBlur={() => setPasswordClicked(true)}
-          />
+            <TextField
+              sx={{
+                marginBottom: "8px",
+                ".MuiFormHelperText-root": {
+                  visibility: !passwordClicked || password ? "hidden" : "visible"
+                }
+              }}
+              required
+              error={!passwordClicked || password ? false: true}
+              id="filled-error-helper-text-required"
+              label="Password"
+              variant="filled"
+              helperText="Password is required"
+              fullWidth
+              type="password"
+              autoComplete="new-password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              onBlur={() => setPasswordClicked(true)}
+            />
 
-          <button className="submitButton" onClick={handleSubmit}>
-            <span className="submitButtonText">Sign in</span>
-          </button>
+            <button className="submitButton" onClick={handleSubmit}>
+              <span className="submitButtonText">Sign in</span>
+            </button>
 
-      </div>
-    </form>
+        </div>
+      </form>
+    </>
   );
 };
 
