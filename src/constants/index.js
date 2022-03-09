dbb830e3d083200f972e7b753703d135bb4e6c03
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-03-09 22:01:01 +0200

    undo last commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,8 +5,6 @@ export const getConfig = {
   }
 };
 
-console.log(process.env.REACT_APP_ENVIRONMENT);
-
 const localhost_backend_api = "http://localhost:5000/api";
 const deploy_backend_api = "https://kolab-backend.herokuapp.com/api";
 
@@ -16,8 +14,8 @@ const deploy_backend = "https://kolab-backend.herokuapp.com";
 const localhost_frontend = "http://localhost:3000";
 const deploy_frontend = "https://kolab-frontend.herokuapp.com";
 
-export const HOST = process.env.REACT_APP_ENVIRONMENT === "production" ? deploy_frontend : localhost_frontend;
+export const HOST = deploy_frontend;
 
-export const SERVER_HOST = process.env.REACT_APP_ENVIRONMENT === "production" ? deploy_backend : localhost_backend;
+export const SERVER_HOST = deploy_backend;
 
-export const SERVER_HOST_API =  process.env.REACT_APP_ENVIRONMENT === "production" ? deploy_backend_api : localhost_backend_api;
+export const SERVER_HOST_API = deploy_backend_api;
