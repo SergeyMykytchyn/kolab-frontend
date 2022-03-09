40c4c6ca2fdf6bf2584e64bc4488106d20cb0517
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-03-09 13:37:09 +0200

    deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,14 +5,17 @@ export const getConfig = {
   }
 };
 
-const localhost_api = "http://localhost:5000/api";
-const deploy_api = "https://kolab-backend.herokuapp.com/api";
+const localhost_backend_api = "http://localhost:5000/api";
+const deploy_backend_api = "https://kolab-backend.herokuapp.com/api";
 
-const localhost = "http://localhost:5000";
-const deploy = "https://kolab-backend.herokuapp.com";
+const localhost_backend = "http://localhost:5000";
+const deploy_backend = "https://kolab-backend.herokuapp.com";
 
-export const HOST = "http://localhost:3000";
+const localhost_frontend = "http://localhost:3000";
+const deploy_frontend = "https://kolab-frontend.herokuapp.com";
 
-export const SERVER_HOST = deploy;
+export const HOST = deploy_frontend;
 
-export const SERVER_HOST_API = deploy_api;
+export const SERVER_HOST = deploy_backend;
+
+export const SERVER_HOST_API = deploy_backend_api;
